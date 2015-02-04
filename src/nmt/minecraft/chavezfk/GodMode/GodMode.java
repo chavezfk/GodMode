a619183a50e197d1a371cdbe9c6cbee355b4a105
chavezfk <chavezfk@gmail.com>
2015-02-04 14:15:03 -0700

    2nd Commit

--- src/nmt/minecraft/chavezfk/GodMode/GodMode.java
+++ src/nmt/minecraft/chavezfk/GodMode/GodMode.java
@@ -3,7 +3,13 @@ package nmt.minecraft.chavezfk.GodMode;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
+/**
+ * 
+ * @author chavezfk (Franz Chavez)
+ * @version 1.0
+ * Saves a player's stats and inventory when they are in survival and changes the player's game mode to creative
+ * then when the player returns to creative, the stats and inventory for their survival mode is restored to them. 
+ */
 public class GodMode extends JavaPlugin implements Listener {
 	@Override 
 	public void onEnable(){
